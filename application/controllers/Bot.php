894a8dd5f18e893bd97ec7e48c0c49477a61d11d
ratihayu <ratih.ayu19@yahoo.com>
2017-06-16 15:35:23 +0700

    first bot

--- application/controllers/Bot.php
+++ application/controllers/Bot.php
@@ -8,24 +8,25 @@ class Bot extends CI_Controller {
     }
 
     $token = "333258187:AAFxzbpduaUH3OT1jDkjeaF7L7p-LfTuj-g";
-    $url="https://api.telegram.org/bot".$token_bot."/";
+    $url="https://api.telegram.org/bot".$token."/";
 
     
     public function index()
     {
-        sendCommand("sendCommand", $token, $data);
+        // sendCommand("sendCommand", $token, $data);
+        $this->getUpdates();
     }
 
-    public function sendCommand($command,$token,array $keterangan=null{
-        $url.=$command."?"; 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $keterangan);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        return $output;
-    }
+    // public function sendCommand($command,$token,array $keterangan=null{
+    //     $url.=$command."?"; 
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL, $url);
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $keterangan);
+    //     $output = curl_exec($ch);
+    //     curl_close($ch);
+    //     return $output;
+    // }
 
     public function getUpdates(){
         $update = file_get_contents($url."getupdates");
@@ -48,27 +49,30 @@ class Bot extends CI_Controller {
 
             $checkCommand = substr($message,0,2);
             if ($checkCommand != "IN") {
-                $warning = "Format pesan salah, coba lagi dengan format INxxx/NAMA CUSTOMER/STATUS(PENDING ATAU CLOSED)/KETERANGAN CLOSED ATAU PENDING"
-                }
-            }
-
-
-            if ($status == "PENDING") {
-                $date = null;
-                $closed_by = null;
-                $hd = null;
-                $keterangan = $keterangan_pending;
-                $keterangan_closed = null;
+                $warning = "Format pesan salah, coba lagi dengan format INxxx/NAMA CUSTOMER/STATUS(PENDING ATAU CLOSED)/KETERANGAN CLOSED ATAU PENDING";
+                $sendCommand = file_get_contents($url."sendmessage?chat_id=".$user_id."&text=".$checkCommand."&reply_to_message_id=".$message_id);
+                json_decode($sendCommand, TRUE);
             }
             else{
-                $hd = "TECH";
-                $closed_by = $user_id;
-                $keterangan = $keterangan_closed;
-                $keterangan_pending = null;
+                if ($status == "PENDING") {
+                    $date = null;
+                    $closed_by = null;
+                    $hd = null;
+                    $keterangan = $keterangan_pending;
+                    $keterangan_closed = null;
+                }
+                else{
+                    $hd = "TECH";
+                    $closed_by = $user_id;
+                    $keterangan = $keterangan_closed;
+                    $keterangan_pending = null;
+                }
+                $this->load->model('Ticket_model');
+                $this->Ticket_model->update($id, $closed_date, $closed_by, $hd, $status, $keterangan_closed, $keterangan_pending);
+                $notif = "Status Ticket telah diubah.";
+                $sendNotif = file_get_contents($url"sendmessage?chat_id=".$user_id."&text=".$notif."&reply_to_message_id=".$message_id);
+                json_decode($sendNotif, TRUE);
             }
-
-            $this->load->model('Ticket_model');
-            $this->Ticket_model->update($id_incident, $date, $closed_by, $hd, $status, $keterangan_closed, $keterangan_pending);
         }
     }
 }
